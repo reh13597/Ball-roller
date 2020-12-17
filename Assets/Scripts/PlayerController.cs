7d8e58b683a2eaf068b27c1f48598a3700d713b4
reh13597 <alexlguo18@gmail.com>
2020-12-16 17:07:35 -0800

    Initial commit of ball version 2

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    public LayerMask groundLayers;
+    public float jumpForce = 7;
+    public SphereCollider col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        col = GetComponent<SphereCollider>();
+
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 30)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movement * speed);
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x,
+            col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+
+            SetCountText();
+        } else if (other.gameObject.CompareTag("PowerUp"))
+        {
+            other.gameObject.SetActive(false);
+            speed = speed + 5;
+        } else if (other.gameObject.CompareTag("PowerUp2"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 10;
+
+            SetCountText();
+        }
+    }
+}
